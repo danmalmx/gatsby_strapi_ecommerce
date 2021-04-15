15d380472e3fe261a470d45d6853ba999c1cd506
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-04-15 14:11:41 +0200

    Design refactoring

--- gsec_frontend/src/components/layout.js
+++ gsec_frontend/src/components/layout.js
@@ -29,14 +29,14 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 640,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
         <footer
           style={{
-            marginTop: `2rem`,
+            marginTop: `42px`,
           }}
         >
           © {new Date().getFullYear()}, Built with
