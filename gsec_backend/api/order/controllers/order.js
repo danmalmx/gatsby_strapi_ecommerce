05c923f3ea00346368b6a42b7e706e6c142d1cf5
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-06-03 14:33:13 +0200

    Sanitize the cart in stripe custom controller

--- gsec_backend/api/order/controllers/order.js
+++ gsec_backend/api/order/controllers/order.js
@@ -9,15 +9,40 @@ const stripe = require('stripe')('sk_test_51Ixw88AGzBGboXkXtoELclMRKfpZX8kfjZJNI
 
 module.exports = {
   setUpStripe: async (ctx) => {
-    let total = 0;
-    let cart = []
+    let total = 100;
+    let validadedCart = []
+    let receiptCart = [];
+
+    const { cart } = ctx.request.body;
+
+    await Promise.all(cart.map(async product => {
+      const validatedProduct = await strapi.services.product.findOne({
+        id: product.id
+      });
+
+      console.log('validatedProduct', validatedProduct);
+
+      if (validatedProduct) {
+        validadedCart.push(validatedProduct);
+        validatedProduct.qty = product.qty;
+
+        receiptCart.push({
+          id: product.id,
+          qty: product.qty
+        })
+      }
+
+
+      return validatedProduct
+    }));
+    console.log('validadedCart', validadedCart)
 
     try {
       const paymentIntent = await stripe.paymentIntents.create({
         amount: total,
         currency: 'eur',
         // Verify your integration in this guide by including this parameter
-        metadata: { cart: JSON.stringify(cart) },
+        metadata: { cart: JSON.stringify(receiptCart) },
       });
 
       return paymentIntent;
